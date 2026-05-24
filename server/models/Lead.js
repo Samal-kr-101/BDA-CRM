fccf7c89784889b227305a36c9dcca6517fc88bf
Samal-kr-101 <samalkr7739@gmail.com>
2026-05-24 10:26:21 +0530

    check files

--- server/models/Lead.js
+++ server/models/Lead.js
@@ -1,20 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const leadSchema = new mongoose.Schema({
-//   companyName: String,
-//   clientName: String,
-//   email: String,
-//   phone: String,
-//   status: {
-//     type: String,
-//     default: "New",
-//   },
-//   notes: String,
-//   assignedTo: String,
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("Lead", leadSchema);
-
 import mongoose from "mongoose";
 
 const leadSchema = new mongoose.Schema(
